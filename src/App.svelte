d2c0fd731f8df836eea8debad62a5366d8c57264
Tony <anthonychung14@gmail.com>
2022-09-23 13:49:52 -0700

    Mint works on rinkeby

--- src/App.svelte
+++ src/App.svelte
@@ -7,28 +7,10 @@
     selectedAccount,
     web3,
   } from "svelte-web3";
-  import * as addressJson from "../constants/contract-address.json";
-  import * as contractJson from "../constants/FourFourFour.json";
-  let count = 0;
+  import * as contractJson from "../constants/npc.json";
 
-  // const rbyDeploy = "0x467FD970C1F33b6f95641f95018301D3427D81FA"
-  // const rbyDeploy = "0xE2055A8D05B849728f46749AdA3AF7A020E06F51";
-  const rbyDeploy = addressJson["FourFourFour"];
-  const store = makeContractStore(contractJson.abi, rbyDeploy);
-
-  // defaultEvmStores.setProvider('https://eth-rinkeby.alchemyapi.io/v2/j0lSD1_wDvtdG-YYUzT_Tceh_95VITDI')
-
-  const errMap = {
-    store: "Store isn't open yet",
-    whitelist: "You must be on the white list",
-    Premint: "Preminting is not open!",
-    Sale: "We haven't opened minting yet",
-    notEnough:
-      "It looks like you don't have enough ETH to cover your mint, please add more to your balance before trying again.",
-  };
-
-  const dateNow = Date.now();
-  const countDownDate = new Date("Feb 8, 2022 15:00:00").getTime();
+  const testnetDeploy = "0xe989b867D924C231894d6Ce0ce29F1F3cAAc9A03";
+  const store = makeContractStore(contractJson.abi, testnetDeploy);
 
   function handleDc() {
     defaultEvmStores.disconnect();
@@ -38,71 +20,36 @@
     defaultEvmStores.setProvider();
   }
 
-  function handleNav() {
-    window.location = "/mint";
-  }
-
   async function handleClickSale() {
-    // alert("Not ready yet!")
-    // return
     try {
-      // console.log("444 activated!");
       const network = await $web3.eth.net.getId();
-      // console.log(network, "NETWORK");
+      console.log(network, "NETWORK");
 
-      const balance = await $web3.eth.getBalance($selectedAccount);
-      // console.log(balance, "BALANCE");
-      const price = await $store.methods.salePrice().call();
-      // console.log(price, "PRICE IS");
-      if (balance < price * count) {
-        alert("You need more ETH!");
-        return;
-      }
-
+      console.log($selectedAccount, "what");
       const gasAmount = await $store.methods
-        .mainSaleMint(count)
-        .estimateGas({ from: $selectedAccount, value: price });
+        .mint($selectedAccount)
+        .estimateGas({ from: $selectedAccount });
 
       const gasPrice = await $web3.eth.getGasPrice();
-      // could estimate gas here
-
       $store.methods
-        .mainSaleMint(count)
+        .mint($selectedAccount)
         .send({
           from: $selectedAccount,
-          value: price,
           gasAmount,
           gasPrice,
         })
         .on("transactionHash", function (hash) {
-          alert("You just minted!");
+          console.log(hash, "we should include a txn hash")
+          alert("Minting successful!")
         })
         .on("error", function (error) {
           if (error.code === 4001) {
             alert("You denied the transaction");
           }
+          console.log(error, "what happened");
         });
     } catch (e) {
       let errMessage;
-      if (e.message.includes("Store")) {
-        errMessage = errMap.store;
-      }
-
-      if (e.message.includes("whitelist")) {
-        errMessage = errMap.whitelist;
-      }
-
-      if (e.message.includes("Preminting")) {
-        errMessage = errMap.Premint;
-      }
-
-      if (e.message.includes("Sale")) {
-        errMessage = errMap.Sale;
-      }
-
-      if (e.message.includes("insufficient funds")) {
-        errMessage = errMap.notEnough;
-      }
 
       if (String(e).includes("Returned values aren't valid")) {
         errMessage =
@@ -117,122 +64,31 @@
       // console.log("bye")
     }
   }
-  async function handleClick() {
-    if (count === 0) {
-      alert("You need to mint at least one");
-      return;
-    }
-
-    return;
-    try {
-      // console.log("444 activated!");
-      const network = await $web3.eth.net.getId();
-      // console.log(network, "NETWORK");
-
-      const balance = await $web3.eth.getBalance($selectedAccount);
-      // console.log(balance, "BALANCE");
-      const price = await $store.methods.premintPrice().call();
-      // console.log(price, "PRICE IS");
-      if (balance < price * count) {
-        alert("You need more ETH!");
-        return;
-      }
-
-      const gasAmount = await $store.methods
-        .presaleMint(count)
-        .estimateGas({ from: $selectedAccount, value: price * count });
-
-      const gasPrice = await $web3.eth.getGasPrice();
-      // could estimate gas here
-
-      $store.methods
-        .presaleMint(count)
-        .send({
-          from: $selectedAccount,
-          value: price * count,
-          gasAmount,
-          gasPrice,
-        })
-        .on("transactionHash", function (hash) {
-          alert("You just minted!");
-        })
-        .on("error", function (error) {
-          if (error.code === 4001) {
-            alert("You denied the transaction");
-          }
-        });
-    } catch (e) {
-      let errMessage;
-      if (e.message.includes("Store")) {
-        errMessage = errMap.store;
-      }
-
-      if (e.message.includes("whitelist")) {
-        errMessage = errMap.whitelist;
-      }
-
-      if (e.message.includes("Preminting")) {
-        errMessage = errMap.Premint;
-      }
-
-      if (e.message.includes("Sale")) {
-        errMessage = errMap.Sale;
-      }
-
-      if (e.message.includes("insufficient funds")) {
-        errMessage = errMap.notEnough;
-      }
-
-      if (String(e).includes("Returned values aren't valid")) {
-        errMessage =
-          "You might be on the wrong network! Make sure you are connected to mainnet!";
-      }
-
-      alert(
-        errMessage ||
-          "Hmm. We ran into an error we didn't recognize. Please let us know and confirm your wallet is connected!"
-      );
-      // console.log(e)
-      // console.log("bye")
-    }
-  }
 
   onMount(() => {
     console.log("setting up dapp");
-    // defaultEvmStores.setProvider();
+    // defaultEvmStores.setProvider(
+    //   "https://eth-rinkeby.alchemyapi.io/v2/j0lSD1_wDvtdG-YYUzT_Tceh_95VITDI"
+    // );
   });
 </script>
 
-<div class="text-center">
+<div class="text-center d-flex justify-content-center">
   {#if !$connected}
-    <button class="itanica-font btn btn-primary mb-5" on:click={handleConnect}>Connect</button>
+    <button
+      class="itanica-font btn btn-primary mb-5"
+      on:click={handleConnect}
+      style="font-size:1em;margin-left:3em;">Connect</button
+    >
   {:else}
     <!-- <button class="itanica-font btn btn-primary mb-5" on:click={handleDc}>Disconnect</button> -->
-    <button class="itanica-font btn btn-primary mb-5" on:click={handleDc}>Mint</button>
+    <button
+      class="itanica-font btn btn-primary mb-5"
+      on:click={handleClickSale}
+      style="font-size:1em;margin-left:3em;">Mint</button
+    >
   {/if}
 </div>
 
 <style>
-
-/* .text-center :global(.btn) {
-  display: inline-block;
-  font-weight: 400;
-  line-height: 1.5;
-  color: #212529;
-  text-align: center;
-  text-decoration: none;
-  vertical-align: middle;
-  cursor: pointer;
-  -webkit-user-select: none;
-     -moz-user-select: none;
-      -ms-user-select: none;
-          user-select: none;
-  background-color: transparent;
-  border: 1px solid transparent;
-  padding: 0.375rem 0.75rem;
-  font-size: 1rem;
-  border-radius: 0.25rem;
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-} */
 </style>
-<!-- https://rinkeby.etherscan.io/address/0xe989b867D924C231894d6Ce0ce29F1F3cAAc9A03 -->
