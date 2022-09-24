fe20205056242679a23399034f0bfc5276d1f59d
Tony <anthonychung14@gmail.com>
2022-09-24 10:25:09 -0700

    Update ABI and check for balance

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import { SvelteToast } from "@zerodevx/svelte-toast";
-  import { confirmToast, failToast } from './Toasts';
+  import { confirmToast, failToast } from "./Toasts";
   import {
     connected,
     defaultEvmStores,
@@ -11,12 +11,13 @@
   } from "svelte-web3";
   import * as contractJson from "../constants/npc.json";
 
-  const testnetDeploy = "0xe989b867D924C231894d6Ce0ce29F1F3cAAc9A03";
-  const store = makeContractStore(contractJson.abi, testnetDeploy);
+  const goerliMinterAddress = "0x97eEBA7e28cfcED7ae2988125EFA857b8E1c2FE2";
+  const store = makeContractStore(contractJson.abi, goerliMinterAddress);
   const options = {};
-  const UNKNOWN_ERROR_MSG = "Hmm. We ran into an error we didn't recognize. Please let us know";
+  const UNKNOWN_ERROR_MSG =
+    "Hmm. We ran into an error we didn't recognize. Please let us know";
   let connectedNetwork = undefined;
-  
+
   function handleDc() {
     defaultEvmStores.disconnect();
   }
@@ -26,23 +27,27 @@
   }
 
   function cleanHash(hash) {
-    return (hash.substring(0,5) + "..." + hash.substring(hash.length-5, hash.length));
+    return (
+      hash.substring(0, 5) +
+      "..." +
+      hash.substring(hash.length - 5, hash.length)
+    );
   }
 
   function createHashLinkedMessage(hash) {
-    let link = '';
-    switch (connectedNetwork){
+    let link = "";
+    switch (connectedNetwork) {
       case 4:
-        link = 'https://rinkeby.etherscan.io/tx/';
+        link = "https://rinkeby.etherscan.io/tx/";
         break;
       case 5:
-        link = 'https://goerli.etherscan.io/tx/';
+        link = "https://goerli.etherscan.io/tx/";
         break;
       default:
-        link = 'https://etherscan.io/tx/';
+        link = "https://etherscan.io/tx/";
     }
     let cleanedHash = cleanHash(hash);
-    return `<a href=${link+hash} target="_blank">${cleanedHash}</a>`;
+    return `<a href=${link + hash} target="_blank">${cleanedHash}</a>`;
   }
 
   async function handleClickSale() {
@@ -50,18 +55,31 @@
       connectedNetwork = await $web3.eth.net.getId();
       console.log(connectedNetwork, "NETWORK");
 
-      console.log($selectedAccount, "what");
-      const gasAmount = await $store.methods
-        .mint($selectedAccount)
-        .estimateGas({ from: $selectedAccount });
+      const balance = (await $web3.eth.getBalance($selectedAccount)) || "";
+      const MINT_QUANTITY = 1;
+
+      const price = await $store.methods
+        .mint_price(MINT_QUANTITY, $selectedAccount)
+        .call();
+
+      if (balance < price * MINT_QUANTITY) {
+        failToast("Sorry. You need more ETH");
+        return;
+      }
 
       const gasPrice = await $web3.eth.getGasPrice();
+      const gasAmount = await $store.methods.mint(MINT_QUANTITY).estimateGas({
+        from: $selectedAccount,
+        value: price,
+      });
+
       $store.methods
-        .mint($selectedAccount)
+        .mint(MINT_QUANTITY)
         .send({
           from: $selectedAccount,
           gasAmount,
           gasPrice,
+          value: price
         })
         .on("transactionHash", function (hash) {
           confirmToast(`Current tx: ${createHashLinkedMessage(hash)}`);
@@ -76,11 +94,13 @@
         })
         .on("receipt", function (blockData) {
           console.log(blockData, "minted!");
-          confirmToast(`Minted NFT!! ${createHashLinkedMessage(blockData.blockHash)}`);
+          confirmToast(
+            `Minted NFT!! ${createHashLinkedMessage(blockData.blockHash)}`
+          );
         });
     } catch (e) {
       let errMessage = UNKNOWN_ERROR_MSG;
-      
+      console.log(e, "error detected");
       if (String(e).includes("Returned values aren't valid")) {
         errMessage =
           "You might be on the wrong network! Make sure you are connected to mainnet!";
@@ -97,10 +117,8 @@
 </script>
 
 <body class="masthead" id="home">
-  <div
-    style="position:absolute; bottom:1em; right: 1em"
-  >
-    <SvelteToast options={options} target={"default"} />
+  <div style="position:absolute; bottom:1em; right: 1em">
+    <SvelteToast {options} target={"default"} />
   </div>
   <div
     class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center"
@@ -130,7 +148,12 @@
     </div>
   </div>
 </body>
-<footer class="footer bg-black small text-center text-white-50" style="background-color: rgb(0, 161, 255);"><div class="container px-4 px-lg-5">watch out mfers</div></footer>
+<footer
+  class="footer bg-black small text-center text-white-50"
+  style="background-color: rgb(0, 161, 255);"
+>
+  <div class="container px-4 px-lg-5">watch out mfers</div>
+</footer>
 
 <style>
   :root {
